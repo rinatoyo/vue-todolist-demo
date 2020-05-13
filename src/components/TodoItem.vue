d87abbff20dcec42f48c18403ca562743d49b77c
Rina Toyoshiba <rtoyoshiba@gmail.com>
2020-05-12 18:57:01 -1000

    first commit, todo and todoitem components already created

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,50 @@
+<!--SECOND COMPONENT CREATION -->
+
+<template>
+    <!-- apply the is-complete style by binding class on a condition of if completed. -->
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <!-- v-on directive is used to add events -->
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <!-- to traverse up to the app, emit the event by using @click. (name, parameter). Parameter is sent as a payload-->
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    // define methods used in the input checkbox here
+    methods: {
+        markComplete(){
+            // changes completed status from true to false or vice versa
+            this.todo.completed = !this.todo.completed
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-complete {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: #ff0000;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,31 @@
+<!--FIRST COMPONENT CREATION -->
+
+<template>
+    <div>
+        <!-- the props an array so you want to loop through them by using v-for.-->
+        <div v-bind:key="todo.id" v-for="todo in todos">
+        <!-- you must also set a key by using v-bind:key or else you will get an elint error. -->
+        
+        <!-- catch the payload from the delete button in todo item component by using v-on -->
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+// import the todo item component here
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: "Todos",
+    // don't forget to add todo item component in export too.
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
